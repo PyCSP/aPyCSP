3d6d21b75920f18a8e101929570d7e337c0ff438
John Markus Bjørndalen <jmb@cs.uit.no>
2018-04-05 15:42:02 +0200

    initial poison implementation

--- apycsp/altimpl.py
+++ apycsp/altimpl.py
@@ -5,6 +5,7 @@
 import asyncio
 import collections
 import functools
+import inspect
 
 class ChannelPoisonException(Exception): 
     pass
@@ -20,6 +21,21 @@ def process(func):
             for ch in [x for x in args if isinstance(x, ChannelEnd) or isinstance(x, Channel)]:
                 await ch.poison()
     return proc_wrapped
+
+def chan_poisoncheck(func):
+    "Decorator for making sure that poisoned channels raise exceptions"
+    # We just need to make sure we can correctly decorate both coroutines and ordinary methods and functions. 
+    @functools.wraps(func)
+    async def p_wrap(self, *args, **kwargs):
+        if self.poisoned:
+            raise ChannelPoisonException()
+        try:
+            return await func(self, *args, **kwargs)
+        finally:
+            if self.poisoned:
+                raise ChannelPoisonException()
+    return p_wrap
+
 
 # TODO: should consider alternative naming of these functions. 
 def run_CSP(*procs):
@@ -94,6 +110,7 @@ class Channel:
             loop = asyncio.get_event_loop()
         self.loop = loop
         self.name = name
+        self.poisoned = False
         self.wqueue = collections.deque()  
         self.rqueue = collections.deque() # could also contain "ALT" ops.
         self.read = ChannelInputEnd(self)
@@ -118,38 +135,94 @@ class Channel:
             rcmd[-1].set_result(obj)
         return (0, obj)
 
-    # TODO: handle poison and retire
-    async def _write(self, obj):
-        wcmd = ['write', obj, None]  # The none is replaced with a future if we have to wait
-        if len(self.wqueue) > 0 or len(self.rqueue) == 0:
-            # a) somebody else is already waiting to write, so we're not going to
-            #    change the situation any with this write. simply append ourselves and wait
-            #    for somebody to wake us up with the result.
-            # b) nobody is waiting for our write. (TODO: buffered channels)
-            return await self._wait_for_op(self.wqueue, wcmd)
-        # find matching read cmd. TODO: need to inspect for alts etc.
-        # if the read _is_ an alt: don't run the rw_nowait, but instead
-        # return a dummy reader object that will be returned by the alt. 
-        # The reader object will then wake up the writer when it actually
-        # reads from the channel.
-        # Alternatively, an alt actually reads and returns both the guard and the
-        # completed operation. Care must be taken to not complete multiple guards if we context switch.... 
-        rcmd = self.rqueue.popleft()
-        return self._rw_nowait(wcmd, rcmd)[0]
-    
-    async def _read(self):
-        rcmd = ['read', None]
-        if len(self.rqueue) > 0 or len(self.wqueue) == 0:
-            # readers ahead of us, or no writiers
-            return await self._wait_for_op(self.rqueue, rcmd)
-        # find matchin write cmd.
-        wcmd = self.wqueue.popleft()
-        return self._rw_nowait(wcmd, rcmd)[1]
-        
-        
+    if 0:
+        # TODO: adding this decorator adds about a microsecond to the op time... Can we improve it? 
+        @chan_poisoncheck
+        async def _write(self, obj):
+            wcmd = ['write', obj, None]  # The none is replaced with a future if we have to wait
+            if len(self.wqueue) > 0 or len(self.rqueue) == 0:
+                # a) somebody else is already waiting to write, so we're not going to
+                #    change the situation any with this write. simply append ourselves and wait
+                #    for somebody to wake us up with the result.
+                # b) nobody is waiting for our write. (TODO: buffered channels)
+                return await self._wait_for_op(self.wqueue, wcmd)
+            # find matching read cmd. TODO: need to inspect for alts etc.
+            # if the read _is_ an alt: don't run the rw_nowait, but instead
+            # return a dummy reader object that will be returned by the alt. 
+            # The reader object will then wake up the writer when it actually
+            # reads from the channel.
+            # Alternatively, an alt actually reads and returns both the guard and the
+            # completed operation. Care must be taken to not complete multiple guards if we context switch.... 
+            rcmd = self.rqueue.popleft()
+            return self._rw_nowait(wcmd, rcmd)[0]
 
+        
+        @chan_poisoncheck
+        async def _read(self):
+            rcmd = ['read', None]
+            if len(self.rqueue) > 0 or len(self.wqueue) == 0:
+                # readers ahead of us, or no writiers
+                return await self._wait_for_op(self.rqueue, rcmd)
+            # find matchin write cmd.
+            wcmd = self.wqueue.popleft()
+            return self._rw_nowait(wcmd, rcmd)[1]
+    else:
+        # For comparison: doing the same without decorators
+        async def _write(self, obj):
+            if self.poisoned:
+                raise ChannelPoisonException()
+            try:
+                wcmd = ['write', obj, None]  # The none is replaced with a future if we have to wait
+                if len(self.wqueue) > 0 or len(self.rqueue) == 0:
+                    return await self._wait_for_op(self.wqueue, wcmd)
+                rcmd = self.rqueue.popleft()
+                return self._rw_nowait(wcmd, rcmd)[0]
+            finally:
+                if self.poisoned:
+                    raise ChannelPoisonException()
+
+        async def _read(self):
+            if self.poisoned:
+                raise ChannelPoisonException()
+            try:
+                rcmd = ['read', None]
+                if len(self.rqueue) > 0 or len(self.wqueue) == 0:
+                    # readers ahead of us, or no writiers
+                    return await self._wait_for_op(self.rqueue, rcmd)
+                # find matchin write cmd.
+                wcmd = self.wqueue.popleft()
+                return self._rw_nowait(wcmd, rcmd)[1]
+            finally:
+                if self.poisoned:
+                    raise ChannelPoisonException()
+    
+
+    async def poison(self):
+        # TODO: this doesn't need to be an async method any longer, but we keep it like this
+        # to make the interface compatible with the baseimpl.
+        if self.poisoned:
+            return
+        self.poisoned = True
+        #self.rwMonitor.notify_all()
+        #if self._ialt:
+        #    # also wake up any input guards.
+        #    await self._ialt.schedule()
+        def poison_queue(queue):
+            while len(queue) > 0:
+                op = queue.popleft()
+                op[-1].set_result(None)
+        poison_queue(self.wqueue)
+        poison_queue(self.rqueue)
+        
+        
 
 One2OneChannel = Channel
 One2AnyChannel = Channel
 Any2OneChannel = Channel
 Any2AnyChannel = Channel
+
+
+async def poisonChannel(ch):
+    "Poisons a channel or a channel end"
+    await ch.poison()
+    
