a5429f05864aee81f9e5791b5ec85b54a36dd007
John Markus Bjørndalen <jmb@cs.uit.no>
2021-03-27 16:31:14 +0100

    reducing linter complaints

--- apycsp/net/__init__.py
+++ apycsp/net/__init__.py
@@ -1,61 +1,61 @@
 #!/usr/bin/env python3
-# -*- coding: latin-1 -*-
-"""PyCSP Barrier based on the JCSP barrier. 
+# -*- coding: utf-8 -*-
+"""PyCSP Barrier based on the JCSP barrier.
 
-Copyright (c) 2018 John Markus Bjørndalen, jmb@cs.uit.no.
-See LICENSE.txt for licensing details (MIT License). 
+Copyright (c) 2018 John Markus BjÃ¸rndalen, jmb@cs.uit.no.
+See LICENSE.txt for licensing details (MIT License).
 
 A simple json based protocol and net implementation for early exeperimentation with a network protocol
-that can be used between processes/event queues. Using a JSON based protocol also lets us 
-interface with Go and Javascript in the future. 
+that can be used between processes/event queues. Using a JSON based protocol also lets us
+interface with Go and Javascript in the future.
 
 There is no attempt at making ALT-capable channels so far.
 
-A rough sketch of how this works is as follows: 
+A rough sketch of how this works is as follows:
 
-- a client wanting to read or write on a channel access a proxy object. 
-- the operation is transformed into a dict / json like encoding of the operation, 
-  and that object is queued on an output queue. 
-  Each operation is given a unique ID to let us match calls with return values. 
-- a message writer picks up the object, encodes it as a json string and sends it to the server. 
+- a client wanting to read or write on a channel access a proxy object.
+- the operation is transformed into a dict / json like encoding of the operation,
+  and that object is queued on an output queue.
+  Each operation is given a unique ID to let us match calls with return values.
+- a message writer picks up the object, encodes it as a json string and sends it to the server.
   Newlines separates messages.
-- the server reads the line with the message, converts it to a dict and spawns a handler that will 
-  find the right channel and run the operation on it. 
-- when the handler completes the operation, it will check for poison and encode the 
-  results (+ poison/exceptions) in a result dict and queue it on the server output queue (for the 
-  respective client connection). 
-- the client receives the message on the channel and spawns a message handler that will look up 
-  the call ID to find the waiting client coroutine. 
+- the server reads the line with the message, converts it to a dict and spawns a handler that will
+  find the right channel and run the operation on it.
+- when the handler completes the operation, it will check for poison and encode the
+  results (+ poison/exceptions) in a result dict and queue it on the server output queue (for the
+  respective client connection).
+- the client receives the message on the channel and spawns a message handler that will look up
+  the call ID to find the waiting client coroutine.
 
-At the moment, the mechanism for waking up a waiting client is to write the result on a 
-per-call queue. We could in principle do the same with a channel, but we have to 
-sort out the poison semantics and potential overheads etc for this first. 
+At the moment, the mechanism for waking up a waiting client is to write the result on a
+per-call queue. We could in principle do the same with a channel, but we have to
+sort out the poison semantics and potential overheads etc for this first.
 
 
-Channel naming service is very simple at the moment: if we don't have an entry for a given channel name, 
-we query all servers for their list of current channels. Then, the channel name is looked up again. 
+Channel naming service is very simple at the moment: if we don't have an entry for a given channel name,
+we query all servers for their list of current channels. Then, the channel name is looked up again.
 
-TODO: 
-- We should consider a more sophisticated naming service. 
-- We should replace the queues with buffered channels. 
+TODO:
+- We should consider a more sophisticated naming service.
+- We should replace the queues with buffered channels.
 """
 
-import apycsp
 import asyncio
 import json
 import functools
-from apycsp import ChannelPoisonException, Channel, ChannelEnd, ChannelReadEnd, ChannelWriteEnd, Parallel
+from apycsp import ChannelPoisonException, Channel, ChannelReadEnd, ChannelWriteEnd, Parallel
 
-# registry of channels to expose to the net. Keys are the names. 
-_chan_registry = {} 
+# registry of channels to expose to the net. Keys are the names.
+_chan_registry = {}
+
 
 def register_channel(chan, name):
     # TODO: should check if already existing
     _chan_registry[name] = chan
 
 
-#############################################################
-# 
+# ############################################################
+#
 # Common handlers. We might consider creating PyCSP procs or channels
 # here instead, but we have to watch for poison (we need to capture
 # and forward it to the client instead of the default behaviour of
@@ -72,30 +72,31 @@ async def _stream_writer(writer, queue):
         if writer.transport.is_closing():
             print("Lost connection on writer before we could send message")
             break
-        res = writer.write(msg_s.encode())
-        res2 = await writer.drain()
+        writer.write(msg_s.encode())
+        await writer.drain()
     writer.close()
     print("_stream_writer done")
 
-    
-async def _stream_reader(reader, handler, wqueue = None):
-    """Reads lines from the reader stream, converts from json strings to objects and runs a handler 
-    for incoming messages. 
+
+async def _stream_reader(reader, handler, wqueue=None):
+    """Reads lines from the reader stream, converts from json strings to objects and runs a handler
+    for incoming messages.
     """
     loop = asyncio.get_event_loop()
     while True:
         data = await reader.readline()
         message = data.decode().strip()
-        if message is None or message is '':
+        if message is None or message == '':
             print("Probably lost client")
             break
         loop.create_task(handler(json.loads(message)))
     print("_stream_reader done")
     if wqueue:
-        await wqueue.put("kill") # put token on output queue to wake up and kill writer
-    
-#############################################################
-# 
+        await wqueue.put("kill")    # put token on output queue to wake up and kill writer
+
+
+# ############################################################
+#
 # Serving clients
 #
 
@@ -108,9 +109,9 @@ async def _stream_reader(reader, handler, wqueue = None):
 # This is something we need to worry about in the DAO project where the other
 # end might disappear permanently or temporarily in the middle of operations.
 # For a temporary disconnect, it's possible to handle some issues by having an ID of the client
-# and reconnect the result queue with the client when it reconnects. 
+# and reconnect the result queue with the client when it reconnects.
 
-async def _handle_cmd(cmd, oqueue = None):
+async def _handle_cmd(cmd, oqueue=None):
     """Interprets and runs the command, waits for and queues the result (including exceptions) on oqueue"""
 
     op = cmd.get('op', None)
@@ -118,13 +119,13 @@ async def _handle_cmd(cmd, oqueue = None):
     if op in ['read', 'write', 'poison']:
         name = cmd.get('name', None)
         chan = _chan_registry.get(name, None)
-        #print("  -- ops is ", op)
+        # print("  -- ops is ", op)
         exc = None
         res = None
         try:
-            # attempt to read or write to a poisoned channel should throw poison 
+            # attempt to read or write to a poisoned channel should throw poison
             if op == 'read':
-                #print(" -- trying to read")
+                # print(" -- trying to read")
                 res = await chan.read()
             elif op == 'write':
                 res = await chan.write(cmd['msg'])
@@ -139,7 +140,7 @@ async def _handle_cmd(cmd, oqueue = None):
     if op == 'ping':
         await oqueue.put({'ack' : msgno, 'ret' : 'ack'})
         return
-    
+
     if op == 'print':
         print("Server asked to print {}".format(cmd['args']))
         await oqueue.put({'ack' : msgno, 'ret' : 'ack'})
@@ -151,22 +152,23 @@ async def _handle_cmd(cmd, oqueue = None):
         print("Returning channel list", chlist)
         await oqueue.put({'ack' : msgno, 'ret' : chlist})
         return
-    
+
     await oqueue.put({'ack' : msgno, 'ret': None, 'err': f'command {op} not recognized'})
     print("handle done")
-    
+
+
 async def _client_handler(reader, writer):
     """Handler for a client connection. Spawns coroutines to handle incoming and outgoing messages."""
-    loop = asyncio.get_event_loop()
+    # loop = asyncio.get_event_loop()
     oqueue = asyncio.Queue()
 
     print("_client_handler creating in/out handler for ", writer.get_extra_info('peername'))
-    await Parallel(_stream_writer(writer, oqueue), 
+    await Parallel(_stream_writer(writer, oqueue),
                    _stream_reader(reader, functools.partial(_handle_cmd, oqueue=oqueue), oqueue))
 
     print("_client handler finished\n")
     # TODO: clean method for noticing a closed connection and notifying the handlers.
-    # see local notes. 
+    # see local notes.
 
 
 def start_server(host_port="8890"):
@@ -179,20 +181,21 @@ def start_server(host_port="8890"):
         port = int(host_port)
     loop = asyncio.get_event_loop()
     serv = asyncio.start_server(_client_handler, host=host, port=port, loop=loop)
-    task = loop.create_task(serv)
+    loop.create_task(serv)
     print("Running server")
     return serv
 
 
 #############################################################
-# 
+#
 # Client side code (for accessing remote channels)
 #
 
 _clconn = {}
-_opqueue = {} # waiting for inputs. 
+_opqueue = {}   # waiting for inputs.
+_msgno = 0
 
-_msgno = 0 
+
 def _get_msgno():
     """Returns a unique message number, for pairing messages with acks"""
     global _msgno
@@ -209,7 +212,7 @@ class _ClientConn():
         self.port = port
         self.wqueue = asyncio.Queue()
         self.rqueue = asyncio.Queue()
-        if loop == None:
+        if loop is None:
             loop = asyncio.get_event_loop()
         self.loop = loop
 
@@ -237,12 +240,12 @@ class _ClientConn():
         res = await rq
         del _opqueue[msgno]  # delete queue after command is finished
         if res.get('exc', None) == 'ChannelPoisonException':
-            #print("propagating poison, ", cmd, res)
+            # print("propagating poison, ", cmd, res)
             raise ChannelPoisonException()
         return res['ret']
-        
 
-async def _setup_client(host_port = '127.0.0.1:8890', loop=None):
+
+async def _setup_client(host_port='127.0.0.1:8890', loop=None):
     host, port = host_port.split(":")
     port = int(port)
     reader, writer = await asyncio.open_connection(host, port, loop=loop)
@@ -252,32 +255,31 @@ async def _setup_client(host_port = '127.0.0.1:8890', loop=None):
     loop.create_task(_stream_writer(writer, conn.wqueue))
     loop.create_task(_stream_reader(reader, conn.handler, conn.wqueue))
 
-    
+
 def setup_client(host_port='127.0.0.1:8890'):
     """Connect to a server. Currently only supports one connection"""
     loop = asyncio.get_event_loop()
     loop.run_until_complete(_setup_client(host_port, loop))
-  
+
 
 # NB: send_message_sync doesn't work when called from a coroutine already executed by the event loop.
 # The event loop is not "reentrant", so you can't provide something that has a "synchronous"
 # external interface and use it from an async function using loop.run_until_complete(co)
 # We have a similar problem with initializing the RemoteChan object. The solution for now is to provide
 # both a synchronous and an asynchronous/couroutine version of get_channel_proxy instead of
-# allocating a RemoteChan object directly. 
+# allocating a RemoteChan object directly.
 def send_message_sync(cmd):
-    """Synchronous send/recv of a message for debug purposes. 
+    """Synchronous send/recv of a message for debug purposes.
     NB: a unique msgno will be inserted into the cmd."""
     loop = asyncio.get_event_loop()
-    conn = _clconn[list(_clconn.keys())[0]]  # TODO: naughty hack 
+    conn = _clconn[list(_clconn.keys())[0]]   # TODO: naughty hack
     return loop.run_until_complete(conn.send_recv_cmd(cmd))
 
 
-        
-# Inherit from Channel and ChannelEnd to make sure poison propagation works locally as well. 
+# Inherit from Channel and ChannelEnd to make sure poison propagation works locally as well.
 class _RemoteChan(Channel):
-    """Proxy object for using a remote channel. 
-    Only supports simple read/write mechanics at the moment. 
+    """Proxy object for using a remote channel.
+    Only supports simple read/write mechanics at the moment.
     Don't allocate this one directly. Use the get_channel_proxy* functions instead.
     """
     def __init__(self, name, loop, conn):
@@ -293,7 +295,7 @@ class _RemoteChan(Channel):
         }
         # TODO: check for poison
         return await self.conn.send_recv_cmd(cmd)
-    
+
     async def _read(self):
         cmd = {
             'op' : 'read',
@@ -301,7 +303,7 @@ class _RemoteChan(Channel):
         }
         # TODO: check for poison
         return await self.conn.send_recv_cmd(cmd)
-    
+
     async def poison(self):
         # Make sure we poison both the local proxy and the remote channel
         await super().poison()
@@ -311,17 +313,17 @@ class _RemoteChan(Channel):
         }
         return await self.conn.send_recv_cmd(cmd)
 
-    
+
 class _RemoteChanProxy(_RemoteChan):
     def __init__(self, name=None, loop=None, conn=None):
-        if loop == None:
+        if loop is None:
             loop = asyncio.get_event_loop()
         super().__init__(name, loop, conn)
         self.read  = ChannelReadEnd(self)
         self.write = ChannelWriteEnd(self)
 
-        
-_rchan_reg = {} # connections to use to reach a remote channel. Indexed by channel name. 
+
+_rchan_reg = {}  # connections to use to reach a remote channel. Indexed by channel name.
 async def _find_remote(name):
     """Find connection for a remote channel. """
     # TODO: this fails if we allow remote channels to move, or if we reconnect to the remote server
@@ -330,8 +332,8 @@ async def _find_remote(name):
     for clname, conn in _clconn.items():
         ret = await conn.send_recv_cmd({'op' : 'chanlist'})
         print("Registering", ret, "as owned by", clname)
-        for name in ret:
-            _rchan_reg[name] = conn
+        for rname in ret:
+            _rchan_reg[rname] = conn
     return _rchan_reg[name]
 
 
--- apycsp/plugNplay/__init__.py
+++ apycsp/plugNplay/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Contains common CSP processes such as Id, Delta, Prefix etc. 
+Contains common CSP processes such as Id, Delta, Prefix etc.
 
 Copyright (c) 2018 John Markus Bjørndalen, jmb@cs.uit.no.
-See LICENSE.txt for licensing details (MIT License). 
+See LICENSE.txt for licensing details (MIT License).
 """
 
-from apycsp import process, Channel, ChannelPoisonException, Alternative, Parallel
+from apycsp import process, Alternative, Parallel
 
 
 @process
@@ -16,13 +16,15 @@ async def Identity(cin, cout):
     to the stream."""
     async for t in cin:
         await cout(t)
-        
+
+
 @process
 async def Prefix(cin, cout, prefixItem=None):
     t = prefixItem
     await cout(t)
     async for t in cin:
         await cout(t)
+
 
 @process
 async def SeqDelta2(cin, cout1, cout2):
@@ -31,25 +33,28 @@ async def SeqDelta2(cin, cout1, cout2):
         await cout1(t)
         await cout2(t)
 
+
 @process
 async def ParDelta2(cin, cout1, cout2):
     """Parallel version of Delta2"""
     async for t in cin:
         # NB: cout1(t) generates a coroutine, which is equivalent with a CSP process.
-        # This is therefore safe to do. 
+        # This is therefore safe to do.
         await Parallel(cout1(t),
                        cout2(t))
 
 # JCSP version uses the parallel version, so we do the same.
 Delta2 = ParDelta2
-        
+
+
 @process
 async def Successor(cin, cout):
     """Adds 1 to the value read on the input channel and outputs it on the output channel.
     Infinite loop.
     """
     async for t in cin:
-        await cout(t+1)
+        await cout(t + 1)
+
 
 @process
 async def SkipProcess():
@@ -60,5 +65,5 @@ async def SkipProcess():
 async def Mux2(cin1, cin2, cout):
     alt = Alternative(cin1, cin2)
     while True:
-        ch, val = await alt.priSelect()
+        _, val = await alt.priSelect()
         await cout(val)
