a16c3dba6eb342af443b164eda360c23aca5c78d
John Markus Bjørndalen <jmb@cs.uit.no>
2023-01-12 00:28:27 +0100

    added benchmarking of channel ends vs direct _read/_write methods

--- benchmarks/producer_consumer.py
+++ benchmarks/producer_consumer.py
@@ -38,27 +38,37 @@ async def consumer(cin, n_warm, n_runs, run_no):
     return per_rw
 
 
-async def run_bm():
+async def run_bm(use_ends=True):
     N_BM = 10
     N_WARM = 100
     N_RUN   = 10_000
     chan = Channel('prod/cons')
 
+    if use_ends:
+        cout = chan.write
+        cin = chan.read
+        bmtype = "channel ends"
+    else:
+        cout = chan._write
+        cin = chan._read
+        bmtype = "_read/_write"
+
     res = []
     for i in range(N_BM):
         rets = await Parallel(
-            producer(chan.write, N_WARM, N_RUN),
-            consumer(chan.read, N_WARM, N_RUN, i))
+            producer(cout, N_WARM, N_RUN),
+            consumer(cin, N_WARM, N_RUN, i))
         # print(rets)
         res.append(rets[-1])
     print("Res with min, avg, max")
     nm_args = " ".join(sys.argv)
-    print(f"| {nm_args} | {min(res):7.3f} | {avg(res):7.3f} |{max(res):7.3f} |")
+    print(f"| {bmtype} | {nm_args} | {min(res):7.3f} | {avg(res):7.3f} |{max(res):7.3f} |")
     return rets
 
 
 if __name__ == "__main__":
     asyncio.run(run_bm())
+    asyncio.run(run_bm(False))
     if args.profile:
         import cProfile
         cProfile.run("run_bm()", sort='tottime')
