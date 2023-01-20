b90332c3a360da7273d6e0175ed73f1308e7adbe
John Markus Bjørndalen <jmb@cs.uit.no>
2023-01-21 00:08:00 +0100

    don't use deprecated tools

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@
 from setuptools import setup
 
 
-
-setup(name='apycsp',
-      version='0.2.0',
-      description='aPyCSP - Python CSP Library using asyncio',
-      author='John Markus Bjørndalen',
-      author_email='jmb@cs.uit.no',
-      url='https://github.com/PyCSP/aPyCSP',
-      license='MIT',
-      packages=['apycsp', 'apycsp.net'],
-      platforms=['any'],
-      )
+setup(
+    name='apycsp',
+    version='0.9.0',
+    description='aPyCSP - Python CSP Library using asyncio',
+    author='John Markus Bjørndalen',
+    author_email='jmb@cs.uit.no',
+    url='https://github.com/PyCSP/aPyCSP',
+    license='MIT',
+    packages=['apycsp', 'apycsp.net'],
+    platforms=['any'],
+)
